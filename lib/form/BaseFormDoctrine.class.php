3bc5e1cc3ddac135c1fb4d96da554fa3a9903d3b
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2008-10-03 12:08:18 -0500

    Use synchronizeWithArray instead of fromArray

--- lib/form/BaseFormDoctrine.class.php
+++ lib/form/BaseFormDoctrine.class.php
@@ -64,4 +64,35 @@ abstract class BaseFormDoctrine extends sfFormDoctrine
 
     return implode("&", $params);
   }
+
+  /**
+   * Updates the values of the object with the cleaned up values.
+   *
+   * @return BaseObject The current updated object
+   */
+  public function updateObject()
+  {
+    if (!$this->isValid())
+    {
+      throw $this->getErrorSchema();
+    }
+
+    $values = $this->getValues();
+
+    // remove special columns that are updated automatically
+    unset($values['id'], $values['updated_at'], $values['updated_on'], $values['created_at'], $values['created_on']);
+
+    // Move translations to the Translation key so that it will work with Doctrine_Record::fromArray()
+    foreach ($this->cultures as $culture)
+    {
+      $translation = $values[$culture];
+      $translation['lang'] = $culture;
+      unset($translation['id']);
+      $values['Translation'][$culture] = $translation;
+      unset($values[$culture]);
+    }
+    $this->object->synchronizeWithArray($values);
+
+    return $this->object;
+  }
 }
