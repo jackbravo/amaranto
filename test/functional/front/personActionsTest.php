b5a91c8bc538c94dec1a17879844795389d74344
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2008-10-03 23:18:20 -0500

    Added tests for person module

--- test/functional/front/personActionsTest.php
+++ test/functional/front/personActionsTest.php
@@ -2,12 +2,89 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-// create a new test browser
-$browser = new sfTestBrowser();
+$b = new sfTestFunctional(new sfBrowser());
 
-$browser->
+$b->
   get('/person/index')->
-  isStatusCode(200)->
-  isRequestParameter('module', 'person')->
-  isRequestParameter('action', 'index')->
-  checkResponseElement('body', '!/This is a temporary page/');
+
+  with('request')->begin()->
+    isParameter('module', 'person')->
+    isParameter('action', 'index')->
+  end()->
+
+  with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('body', '!/This is a temporary page/')->
+  end()
+;
+
+// test edit page
+$b->info('test edit page')
+  ->get('/person/create')
+  ->with('response')->isStatusCode()
+  ->with('request')->begin()
+    ->isParameter('module', 'person')
+    ->isParameter('action', 'create')
+    ->isParameter('id', false)
+  ->end()
+;
+
+// test insert Entities
+$test_person = array('person' => array(
+  'name' => 'Joaquin Bravo',
+  'Emails' => array(
+    array('email' => 'jackbravo@gmail.com', 'type' => 1),
+  ),
+));
+$b->info('test insert person')
+  ->click('Save', $test_person)
+  ->with('form')->begin()
+    ->hasErrors(false)
+  ->end()
+  ->isRedirected()
+  ->followRedirect()
+  ->with('response')->begin()
+    ->checkElement('form input[name="person[name]"][value="Joaquin Bravo"]', true)
+    ->checkElement('form #emails input[value="jackbravo@gmail.com"]', true)
+  ->end()
+  ->test()->like($b->getRequest()->getParameter('id'), '/\d+/', 'Id is number')
+;
+
+$new_person_id = $b->getRequest()->getParameter('id');
+
+// test validation
+$test_person['person']['name'] = '';
+$test_person['id'] = $new_person_id;
+$b->info('test basic validation and form refill')
+  ->get('/person/edit/' . $new_person_id)
+  ->click('Save', $test_person)
+  ->with('form')->begin()
+    ->hasErrors(1)
+    ->isError('name', '/Required/')
+  ->end()
+  ->with('response')->begin()
+    ->checkElement('form input[name="person[name]"][value=""]', true)
+    ->checkElement('form #emails input[value="jackbravo@gmail.com"]', true)
+  ->end()
+;
+
+$testCases = sfYaml::load(dirname(__FILE__).'/../../fixtures/create_entities.yml');
+
+foreach ($testCases['Entity'] as $key => $person)
+{
+  $b->info("Saving $key")
+    ->get('/person/create')
+    ->click('Save', array('person' => $person))
+    ->with('form')->hasErrors(false)
+    ->isRedirected()
+    ->followRedirect()
+  ;
+}
+
+// TODO: missing testing for succesful deletion
+$b->info('test delete person')
+  ->get('/person/edit/id/' . $new_person_id)
+  ->click('Delete')
+  ->isRedirected()
+  ->followRedirect()
+;
