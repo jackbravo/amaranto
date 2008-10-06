3740d780610ebf179c2f92c6c8b4a5cf93f6c9b7
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2008-10-06 17:44:04 -0500

    Changed form closure for divs instead of td's

--- apps/front/modules/person/templates/editSuccess.php
+++ apps/front/modules/person/templates/editSuccess.php
@@ -64,15 +64,12 @@
 <?php endforeach; ?>
 </div>
 
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          &nbsp;<a href="<?php echo url_for('person/index') ?>">Cancel</a>
-          <?php if (!$form->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'person/delete?id='.$person->get('id'), array('post' => true, 'confirm' => 'Are you sure?')) ?>
-          <?php endif; ?>
-          <input type="submit" value="Save" />
-        </td>
-      </tr>
-    </tfoot>
+<div>
+  &nbsp;<a href="<?php echo url_for('person/index') ?>">Cancel</a>
+  <?php if (!$form->isNew()): ?>
+    &nbsp;<?php echo link_to('Delete', 'person/delete?id='.$person->get('id'), array('post' => true, 'confirm' => 'Are you sure?')) ?>
+  <?php endif; ?>
+  <input type="submit" value="Save" />
+</div>
+
 </form>
