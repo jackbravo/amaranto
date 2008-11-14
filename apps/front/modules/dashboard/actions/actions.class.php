da045c4c409c76517a4f1aea3396952bcaf5eac9
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2008-11-14 12:39:03 -0600

    added default module: dashboard

---
+++ apps/front/modules/dashboard/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * dashboard actions.
+ *
+ * @package    cms
+ * @subpackage dashboard
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class dashboardActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
