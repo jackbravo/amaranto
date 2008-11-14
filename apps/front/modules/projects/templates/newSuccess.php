a9954d655bf24078acfabaf8d9ed9a04e86d30f6
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2008-11-14 13:33:45 -0600

    Updated new project form

--- apps/front/modules/projects/templates/newSuccess.php
+++ apps/front/modules/projects/templates/newSuccess.php
@@ -1,3 +1,15 @@
 <h1>New Projects</h1>
 
-<?php include_partial('form', array('form' => $form)) ?>
+<?php echo form_tag_for($form, '@projects') ?>
+<?php echo $form->renderGlobalErrors() ?>
+<?php echo $form->renderHiddenFields() ?>
+
+<?php echo $form['name']->renderRow() ?>
+<?php echo $form['description']->renderRow() ?>
+
+<div>
+  &nbsp;<a href="<?php echo url_for('@projects') ?>">Cancel</a>
+  <input type="submit" value="Save" />
+</div>
+
+</form>
