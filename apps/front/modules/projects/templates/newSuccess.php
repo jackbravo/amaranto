b5d250f8f1d5017628f16b7efe466c3c944e728e
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2008-12-22 11:21:39 -0600

    autocomplete

--- apps/front/modules/projects/templates/newSuccess.php
+++ apps/front/modules/projects/templates/newSuccess.php
@@ -1,3 +1,6 @@
+<?php use_javascript('ui/ui.core.js') ?>
+<?php use_javascript('ui/ui.autocomplete.js') ?>
+<?php use_stylesheet('jquery-ui-south.css') ?>
 <h1>New Projects</h1>
 
 <?php echo form_tag_for($form, '@projects') ?>
@@ -5,6 +8,20 @@
 <?php echo $form->renderHiddenFields() ?>
 
 <?php echo $form['name']->renderRow() ?>
+<?php echo $form['entity']->renderRow() ?>
+<script type="text/javascript">
+  $("#project_entity").autocomplete({
+    url: "<?php echo url_for("parties/ajaxList") ?>",
+    dataType: "json",
+    parse:    function(data) {
+      var parsed = [];
+      for (key in data) {
+        parsed[parsed.length] = { data: [ data[key], key ], value: data[key], result: data[key] };
+      }
+      return parsed;
+    }
+  });
+</script>
 <?php echo $form['description']->renderRow() ?>
 
 <div>
--- lib/form/doctrine/ProjectForm.class.php
+++ lib/form/doctrine/ProjectForm.class.php
@@ -14,6 +14,30 @@ class ProjectForm extends BaseProjectForm
     $this->widgetSchema['name']->setAttributes(array('size' => '40'));
     $this->widgetSchema['description'] = new sfWidgetFormTextarea();
 
+    $this->widgetSchema['entity'] = new sfWidgetFormInput();
+    $this->validatorSchema['entity'] = new sfValidatorString(array(
+      'max_length' => 255,
+      'required' => false,
+    ));
+
+    $this->setDefault('entity', $this->object->Entity->name);
+
+    // this field is required to avoid deleting the Company
+    // record with the synchronizeWithArray method
+    $this->widgetSchema['Entity'] = new sfWidgetFormInputHidden();
+    $this->validatorSchema['Entity'] = new sfValidatorString(array(
+      'required' => false,
+    ));
+
     unset($this['created_at'], $this['updated_at']);
+  }
+
+  public function updateObject($values = null)
+  {
+    $object = parent::updateObject(null);
+
+    $this->updateEntity();
+
+    return $object;
   }
 }
