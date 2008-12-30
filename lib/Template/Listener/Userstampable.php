b30dd346ee0d11d7eab361b1b46d42358c6538c0
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2008-12-30 15:08:15 -0600

    Added Usertampable tempalte

---
+++ lib/Template/Listener/Userstampable.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Doctrine_Template_Listener_Timestampable
+ *
+ * @package     Doctrine
+ * @subpackage  Template
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.phpdoctrine.com
+ * @since       1.0
+ * @version     $Revision$
+ * @author      Konsta Vesterinen <kvesteri@cc.hut.fi>
+ */
+class Listener_Userstampable extends Doctrine_Record_Listener
+{
+  /**
+   * Array of timestampable options
+   *
+   * @var string
+   */
+  protected $_options = array();
+
+  /**
+   * __construct
+   *
+   * @param string $options
+   * @return void
+   */
+  public function __construct(array $options)
+  {
+    $this->_options = $options;
+  }
+
+  /**
+   * preInsert
+   *
+   * @param object $Doctrine_Event
+   * @return void
+   */
+  public function preInsert(Doctrine_Event $event)
+  {
+    $createdName = $this->_options['created']['name'];
+    $updatedName = $this->_options['updated']['name'];
+
+    $event->getInvoker()->$createdName = $this->getCurrentUserId();
+    $event->getInvoker()->$updatedName = $this->getCurrentUserId();
+  }
+
+  /**
+   * preUpdate
+   *
+   * @param object $Doctrine_Event
+   * @return void
+   */
+  public function preUpdate(Doctrine_Event $event)
+  {
+    $updatedName = $this->_options['updated']['name'];
+
+    $event->getInvoker()->$updatedName = $this->getCurrentUserId();
+  }
+
+  /**
+   * getTimestamp
+   *
+   * Gets the timestamp in the correct format
+   *
+   * @param string $type
+   * @return void
+   */
+  public function getCurrentUserId()
+  {
+    try
+    {
+      return sfContext::getInstance()->getUser()
+        ->getAttribute('user_id', null, 'sfGuardSecurityUser');
+    }
+    catch (Exception $e)
+    {
+      return null;
+    }
+  }
+}
---
+++ lib/Template/Userstampable.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Easily add created and updated at timestamps to your doctrine records
+ */
+class Userstampable extends Doctrine_Template
+{
+  /**
+   * Array of timestampable options
+   *
+   * @var string
+   */
+  protected $_options = array(
+    'created' => array(
+      'name'     => 'created_by_user_id',
+      'type'     => 'integer',
+      'length'   => '4',
+      'options'  => array(),
+      'disabled' => false,
+    ),
+    'updated' => array(
+      'name'    => 'updated_by_user_id',
+      'type'    => 'integer',
+      'length'  => '4',
+      'options' => array(),
+      'disabled' => false,
+    )
+  );
+
+  /**
+   * __construct
+   *
+   * @param string $array
+   * @return void
+   */
+  public function __construct(array $options = array())
+  {
+    $this->_options = Doctrine_Lib::arrayDeepMerge($this->_options, $options);
+  }
+
+  /**
+   * setTableDefinition
+   *
+   * @return void
+   */
+  public function setTableDefinition()
+  {
+    if( ! $this->_options['created']['disabled']) {
+      $this->hasColumn($this->_options['created']['name'],
+        $this->_options['created']['type'],
+        $this->_options['created']['length'],
+        $this->_options['created']['options']);
+    }
+    if( ! $this->_options['updated']['disabled']) {
+      $this->hasColumn($this->_options['updated']['name'],
+        $this->_options['updated']['type'],
+        $this->_options['updated']['length'],
+        $this->_options['updated']['options']);
+    }
+    $this->addListener(new Listener_Userstampable($this->_options));
+  }
+
+  public function setUp()
+  {
+    $this->hasOne('sfGuardUser as CreatedBy', array(
+      'local' => $this->_options['created']['name'],
+      'foreign' => 'id'));
+
+    $this->hasOne('sfGuardUser as UpdatedBy', array(
+      'local' => $this->_options['updated']['name'],
+      'foreign' => 'id'));
+  }
+}
