b32ee45b60ac101300f5ea936e90f445d728c107
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2009-01-08 14:15:38 -0600

    change the show bug page

--- apps/front/modules/issues/templates/showSuccess.php
+++ apps/front/modules/issues/templates/showSuccess.php
@@ -1,90 +1,68 @@
-<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $issue->getid() ?></td>
-    </tr>
-    <tr>
-      <th>Title:</th>
-      <td><?php echo $issue->gettitle() ?></td>
-    </tr>
-    <tr>
-      <th>Project:</th>
-      <td><?php echo $issue->getproject_id() ?></td>
-    </tr>
-    <tr>
-      <th>Component:</th>
-      <td><?php echo $issue->getcomponent_id() ?></td>
-    </tr>
-    <tr>
-      <th>Assigned to:</th>
-      <td><?php echo $issue->getassigned_to() ?></td>
-    </tr>
-    <tr>
-      <th>Is open:</th>
-      <td><?php echo $issue->getis_open() ?></td>
-    </tr>
-    <tr>
-      <th>Opened at:</th>
-      <td><?php echo $issue->getopened_at() ?></td>
-    </tr>
-    <tr>
-      <th>Opened by:</th>
-      <td><?php echo $issue->getopened_by() ?></td>
-    </tr>
-    <tr>
-      <th>Resolved at:</th>
-      <td><?php echo $issue->getresolved_at() ?></td>
-    </tr>
-    <tr>
-      <th>Resolved by:</th>
-      <td><?php echo $issue->getresolved_by() ?></td>
-    </tr>
-    <tr>
-      <th>Closed at:</th>
-      <td><?php echo $issue->getclosed_at() ?></td>
-    </tr>
-    <tr>
-      <th>Closed by:</th>
-      <td><?php echo $issue->getclosed_by() ?></td>
-    </tr>
-    <tr>
-      <th>Status:</th>
-      <td><?php echo $issue->getstatus_id() ?></td>
-    </tr>
-    <tr>
-      <th>Category:</th>
-      <td><?php echo $issue->getcategory_id() ?></td>
-    </tr>
-    <tr>
-      <th>Priority:</th>
-      <td><?php echo $issue->getpriority_id() ?></td>
-    </tr>
-    <tr>
-      <th>Milestone:</th>
-      <td><?php echo $issue->getmilestone_id() ?></td>
-    </tr>
-    <tr>
-      <th>Orig estimate:</th>
-      <td><?php echo $issue->getorig_estimate() ?></td>
-    </tr>
-    <tr>
-      <th>Curr estimate:</th>
-      <td><?php echo $issue->getcurr_estimate() ?></td>
-    </tr>
-    <tr>
-      <th>Elapsed:</th>
-      <td><?php echo $issue->getelapsed() ?></td>
-    </tr>
-    <tr>
-      <th>Deadline:</th>
-      <td><?php echo $issue->getdeadline() ?></td>
-    </tr>
-  </tbody>
-</table>
+<?php use_helper('Text') ?>
+<div class="grid_8">
 
-<hr />
+  <h1><?php echo $issue->title ?> <small>(<?php echo $issue->Category ?>)</small></h1>
+  Assigned to <strong><?php echo $issue->AssignedTo ?></strong>
+  | Priority - <strong><?php echo $issue->Priority->id.' '.$issue->Priority ?></strong>
 
-<a href="<?php echo url_for('issues_edit', $issue) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('issues') ?>">List</a>
+  <hr />
+
+  <div class="issue-activities">
+  <?php foreach ($issue->Activities as $activity): ?>
+    <div class="issue-activity">
+      <p class="activity-timestamp">
+        <strong><?php echo $activity->verb ?> by <?php echo $activity->CreatedBy ?></strong>
+        <small><?php echo $activity->created_at ?></small>
+      </p>
+      <blockquote class="activity-changes">
+        <?php echo simple_format_text($activity->changes) ?>
+      </blockquote>
+      <?php echo simple_format_text($activity->body) ?>
+    </div>
+  <?php endforeach; ?>
+  </div> <!-- /issue-history -->
+
+  <hr />
+
+  <a href="<?php echo url_for('issues_edit', $issue) ?>">Edit</a>
+  &nbsp;
+  <a href="<?php echo url_for('issues') ?>">List</a>
+
+</div> <!-- /grid_8 -->
+
+<div class="grid_4">
+  <div class="issue-id-box">
+    <h1 class="issue-id">
+      <?php echo link_to($issue->id, 'issues_show', $issue) ?>
+    </h1>
+    <strong class="issue-status"><?php echo $issue->Status ?></strong>
+  </div>
+
+  <div class="issue-location">
+    <?php if ($issue->Project->exists()): ?>
+      <strong>Project.</strong> <?php echo $issue->Project ?><br/>
+    <?php endif; ?>
+    <?php if ($issue->Component->exists()): ?>
+      <strong>Component.</strong> <?php echo $issue->Component ?><br/>
+    <?php endif; ?>
+    <?php if ($issue->Milestone->exists()): ?>
+      <strong>Milestone.</strong> <?php echo $issue->Milestone ?><br/>
+    <?php endif; ?>
+  </div>
+
+  <br/>
+  <div class="issue-estimate">
+    <?php if ($issue->curr_estimate): ?>
+      <strong>Estimate.</strong> <?php echo $issue->curr_estimate ?><br/>
+    <?php endif; ?>
+    <?php if ($issue->elapsed): ?>
+      <strong>Elapsed.</strong> <?php echo $issue->elapsed ?><br/>
+      <strong>Remaining.</strong> <?php echo $issue->curr_estimate - $issue->elapsed ?><br/>
+    <?php endif; ?>
+  </div>
+
+  <br/>
+  <?php if ($issue->deadline): ?>
+    <strong>Deadline.</strong> <?php echo $issue->deadline ?><br/>
+  <?php endif; ?>
+</div> <!-- /grid_4 -->
