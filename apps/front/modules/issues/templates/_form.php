ba8be4763aad7f35aba3e1ceeb98843ec0e7b98a
Joaquin Bravo Contreras <jackbravo@thundercat2.(none)>
2009-01-08 00:28:12 -0600

    some formatting (still more to go) for issue form

--- apps/front/modules/issues/templates/_form.php
+++ apps/front/modules/issues/templates/_form.php
@@ -3,12 +3,39 @@
 
 <?php echo form_tag_for($form, '@issues') ?>
 
-  <?php echo $form ?>
+  <?php echo $form->renderGlobalErrors() ?>
+  <?php echo $form->renderHiddenFields() ?>
 
-  &nbsp;<a href="<?php echo url_for('issues') ?>">Cancel</a>
-  <?php if (!$form->getObject()->isNew()): ?>
-    &nbsp;<?php echo link_to('Delete', 'issues_delete', $form->getObject(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-  <?php endif; ?>
-  <input type="submit" value="Save" />
+  <div class="fullspan clearfix">
+    <div class="grid_6"><?php echo $form['title']->renderRow() ?></div>
+    <div class="grid_3"><?php echo $form['project_id']->renderRow() ?></div>
+    <div class="grid_3"><?php echo $form['component_id']->renderRow() ?></div>
+  </div>
+
+  <div class="fullspan clearfix">
+    <div class="grid_6"><?php echo $form['assigned_to']->renderRow() ?></div>
+    <div class="grid_3"><?php echo $form['priority_id']->renderRow() ?></div>
+    <div class="grid_3"><?php echo $form['milestone_id']->renderRow() ?></div>
+  </div>
+
+  <div class="fullspan clearfix">
+    <div class="grid_6"><?php echo $form['Activities'][0]['body']->renderRow() ?></div>
+    <div class="grid_6">
+      <?php echo $form['category_id']->renderRow() ?>
+      <?php echo $form['status_id']->renderRow() ?>
+      <?php echo $form['curr_estimate']->renderRow() ?>
+      <?php echo $form['elapsed']->renderRow() ?>
+      <?php echo $form['deadline']->renderRow() ?>
+    </div>
+  </div>
+
+  <br/>
+  <div class="grid_12">
+    &nbsp;<a href="<?php echo url_for('issues') ?>">Cancel</a>
+    <?php if (!$form->getObject()->isNew()): ?>
+      &nbsp;<?php echo link_to('Delete', 'issues_delete', $form->getObject(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+    <?php endif; ?>
+    <input type="submit" value="Save" />
+  </div>
 
 </form>
