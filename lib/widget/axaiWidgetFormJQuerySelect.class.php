02e168d3ddb0146f4fd8d08e091bce8b08cfd95b
Joaquin Bravo <jackbravo@axai1.(none)>
2009-02-06 09:47:59 -0600

    Use the add_empty option for the select ajax widget

--- lib/widget/axaiWidgetFormJQuerySelect.class.php
+++ lib/widget/axaiWidgetFormJQuerySelect.class.php
@@ -12,14 +12,23 @@ class axaiWidgetFormJQuerySelect extends sfWidgetFormDoctrineSelect
 
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
-    // TODO: add empty
+    if (false !== $empty = $this->getOption('add_empty'))
+    {
+      $label = true === $empty ? '' : $empty;
+      $first_option = '<option value="">' . $label . '</option>';
+    }
+    else
+    {
+      $first_option = '';
+    }
+
     return parent::render($name, $value, $attributes, $errors).
            sprintf("
 <script type='text/javascript'>
 \$(function(){
   \$('#%s').change(function(){
     \$.getJSON('%s', { id: \$(this).val() }, function(data){
-      var options = '';
+      var options = '%s';
       for (key in data) {
         options += '<option value=\"' + key + '\">' + data[key] + '</option>';
       }
@@ -32,6 +41,7 @@ class axaiWidgetFormJQuerySelect extends sfWidgetFormDoctrineSelect
       ,
       $this->generateId($this->getOption('parent')),
       $this->getOption('url'),
+      $first_option,
       $this->generateId($name)
     );
   }
